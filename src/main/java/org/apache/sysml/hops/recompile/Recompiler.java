d10ad8729f9e812689dd230101c0c20f456a8d4b
Jonathan Leang <jleang@cs.washington.edu>
2019-10-22 14:18:47 -0700

    parse DataGenOp debug

--- src/main/java/org/apache/sysml/hops/recompile/Recompiler.java
+++ src/main/java/org/apache/sysml/hops/recompile/Recompiler.java
@@ -363,6 +363,7 @@ public class Recompiler
 			Hop.resetVisitStatus(hops);
 		}
 
+		new Exception().printStackTrace();
 		if (ConfigurationManager.isWormholeEnabled()) {
 			Hop.resetVisitStatus(hops);
 			hops = Wormhole.optimize(hops);
