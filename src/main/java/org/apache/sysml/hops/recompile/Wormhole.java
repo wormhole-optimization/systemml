49ef611858dd78fd9d800c2ac1fcf93b9cf05112
Jonathan Leang <jleang@cs.washington.edu>
2019-10-22 17:14:41 -0700

    no cache des debug

--- src/main/java/org/apache/sysml/hops/recompile/Wormhole.java
+++ src/main/java/org/apache/sysml/hops/recompile/Wormhole.java
@@ -30,15 +30,6 @@ import org.apache.sysml.hops.Hop.OpOp3;
 import org.apache.sysml.hops.Hop.OpOp4;
 import org.apache.sysml.hops.Hop.OpOpN;
 import org.apache.sysml.hops.Hop.ReOrgOp;
-import org.apache.sysml.hops.IndexingOp;
-import org.apache.sysml.hops.LeftIndexingOp;
-import org.apache.sysml.hops.LiteralOp;
-import org.apache.sysml.hops.NaryOp;
-import org.apache.sysml.hops.OptimizerUtils;
-import org.apache.sysml.hops.QuaternaryOp;
-import org.apache.sysml.hops.ReorgOp;
-import org.apache.sysml.hops.TernaryOp;
-import org.apache.sysml.hops.UnaryOp;
 import org.apache.sysml.parser.DataIdentifier;
 import org.apache.sysml.parser.Expression;
 import org.apache.sysml.parser.Expression.ValueType;
@@ -48,8 +39,8 @@ import scala.Tuple2;
 public class Wormhole {
     private static final boolean LOG_WARP_STD = true;
     private static final boolean LOG_WARP_ERR = true;
-    private static final boolean LOG_DES = false;
-    private static final boolean USE_MEGACACHE = true;
+    private static final boolean LOG_DES = true;
+    private static final boolean USE_MEGACACHE = false;
     private static final String HOPS_FOLDER = "/tmp";
 
     /**
